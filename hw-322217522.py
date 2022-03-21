7fbdab9612f907a0547128e26933b7208eb62dc0
aviel12 <100856699+aviel12@users.noreply.github.com>
2022-03-21 22:24:18 +0200

    Add files via upload

--- hw-322217522.py
+++ hw-322217522.py
@@ -1,18 +1,4 @@
 # A
-with open('ex3_text.txt') as f:
-    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
-
-    str_f = str(f.read())
-    f_rep = str_f.replace('.', ' ').replace('-', ' ').replace(',', ' ').replace('(', ' ').replace(')', ' ')
-    list_f = f_rep.split()
-    longest_list = []
-    while len(longest_list) < 5:
-        max_word = max(list_f, key=len)
-        longest_list.append(max_word)
-        list_f.remove(max_word)
-
-
-# B
 def read_line(n, file):
     if type(n) is not int:
         return "invalid input detected"
@@ -29,3 +15,17 @@ def read_line(n, file):
 
     except:
         return "file not found"
+
+
+# B
+def longest_words(file):
+    with open(file) as f:
+        str_f = str(f.read())
+        f_rep = str_f.replace('.', ' ').replace('-', ' ').replace(',', ' ').replace('(', ' ').replace(')', ' ')
+        list_f = f_rep.split()
+        longest_list = []
+        while len(longest_list) < 5:
+            max_word = max(list_f, key=len)
+            longest_list.append(max_word)
+            list_f.remove(max_word)
+        return longest_list
